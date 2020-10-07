1e50aa67bcf2456862541a5f201ee9eda7809fff
Zach <zweebees@gmail.com>
2020-10-07 04:27:16 -0400

    Bug Fix
    Fixed small bug with timer

--- game.js
+++ game.js
@@ -2,37 +2,43 @@
 var canvas = document.getElementById("draw");
 var ctx = canvas.getContext("2d");
 var strokeColor = "#000";
+var gameActive = false;
 
 function startGame() {
-  document.getElementById("countdown").textContent = 45; //Resets to 45 if restarting the game
-  //893 pokemon, so generate random number from 1 to 893
-  var rand = Math.floor((Math.random() * 893) + 1).toString();
-  //URL uses 3 digits, so this will generate zeroes before numbers if they are 1 or 2 digits
-  if (rand.length == 1) {
-    rand = "00" + rand;
-  }
-  if (rand.length == 2) {
-    rand = "0" + rand;
-  }
-  var sourceString = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + rand + ".png";
-  //Change image
-  document.getElementById("image").src = sourceString;
+  //Check to see if game is already started
+  if (gameActive == false) {
+    gameActive = true;
+    document.getElementById("countdown").textContent = 45; //Resets to 45 if restarting the game
+    //893 pokemon, so generate random number from 1 to 893
+    var rand = Math.floor((Math.random() * 893) + 1).toString();
+    //URL uses 3 digits, so this will generate zeroes before numbers if they are 1 or 2 digits
+    if (rand.length == 1) {
+      rand = "00" + rand;
+    }
+    if (rand.length == 2) {
+      rand = "0" + rand;
+    }
+    var sourceString = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + rand + ".png";
+    //Change image
+    document.getElementById("image").src = sourceString;
 
-  initCanvas();
+    initCanvas();
 
-  //Timer
-  var seconds = document.getElementById("countdown").textContent;
-  var countdown = setInterval(function() {
-      seconds--;
-      document.getElementById("countdown").textContent = seconds;
-      //When timer hits 0, stop the countdown and remove the event listeners
-      if (seconds <= 0) {
-        clearInterval(countdown);
-        canvas.removeEventListener("mousedown", mouseDown);
-        canvas.removeEventListener("mousemove", mouseMove);
-        canvas.removeEventListener("mouseup", mouseUp);
-      }
-  }, 1000);
+    //Timer
+    var seconds = document.getElementById("countdown").textContent;
+    var countdown = setInterval(function() {
+        seconds--;
+        document.getElementById("countdown").textContent = seconds;
+        //When timer hits 0, stop the countdown and remove the event listeners
+        if (seconds <= 0) {
+          clearInterval(countdown);
+          canvas.removeEventListener("mousedown", mouseDown);
+          canvas.removeEventListener("mousemove", mouseMove);
+          canvas.removeEventListener("mouseup", mouseUp);
+          gameActive = false;
+        }
+    }, 1000);
+  }
 }
 
 //Canvas
